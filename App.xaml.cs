121ba0f2553d2a62aecfe8342172fefa2db62f74
Mateo Cisneros <138953180+mateo2408@users.noreply.github.com>
2025-07-07 07:21:57 -0500

    Initial Commit

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Cisneros_ExamenP3;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Cisneros_ExamenP3;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
